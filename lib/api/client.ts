b18d5ba212664af1c1779f9912db0828bf4ddc67
Ala Gowtham Siva Kumar <111774754+gowtham-2oo5@users.noreply.github.com>
2025-12-19 08:20:47 +0530

    Update API base URL to new HTTPS endpoint

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "https://ec2-35-170-179-81.compute-1.amazonaws.com";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://crt-portal-api.gows.me/";
 const API_TIMEOUT = 10000;
 const BULK_UPLOAD_API_TIMEOUT = 300000;
 
